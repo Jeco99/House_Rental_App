e6ba995bca37c83c923009c6772ac966a52382bf
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-10-02 23:36:23 +0800

    Add component home and housinglocation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: '<h1>Hello world</h1>',
+//   styleUrls: ['./app.component.css']
+// })
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:` <main> <header class="brand-name"> <img class="brand-logo" src="/assets/logo.svg" alt="logo"
+  aria-hidden="true"> </header> <section class="content"> <app-home></app-home> </section> </main> `,
 })
+
 export class AppComponent {
   title = 'House_Rental_App';
 }
