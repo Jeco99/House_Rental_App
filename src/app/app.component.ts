33c890c327ad3cc4ce800c3f0ec576cdea8f28dd
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-10-03 18:13:12 +0800

    house_rental_app part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template:` <main> <header class="brand-name"> <img class="brand-logo" src="/assets/logo.svg" alt="logo"
   aria-hidden="true"> </header> <section class="content"> <app-home></app-home> </section> </main> `,
+  styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent {
